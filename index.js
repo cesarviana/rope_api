e14e3d0ea88e41bde52095c746f0520cbdeb00f4
Cesar Pereira Viana <cesar.pviana@gmail.com>
2020-08-08 18:06:00 -0300

    Project running

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "*"
+};
+  
+app.use(cors(corsOptions));
+app.use(bodyParser.json())
+
+app.get('/', (_, res)=> res.send('Hello API'))
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}.`);
+});
